35ebe00eaae03889132d82d75477ee92fc46ab01
Dev GJ Pro <devgjpro@gmail.com>
2025-06-05 22:09:43 +0530

    added bat

--- main/train.bat
+++ main/train.bat
@@ -9,6 +9,6 @@ set batch_test=32
 set lr=1e-3
 set lr_decay=0.99
 set decay_interval=10
-set iteration=10
+set iteration=2
 set setting=%dataset%--%property%--dim%dim%--layer_hidden%layer_hidden%--layer_output%layer_output%--batch_train%batch_train%--batch_test%batch_test%--lr%lr%--lr_decay%lr_decay%--decay_interval%decay_interval%--iteration%iteration
 python train.py %dataset% "U0(kcalmol^-1)" %dim% %layer_hidden% %layer_output% %batch_train% %batch_test% %lr% %lr_decay% %decay_interval% %iteration% %setting%
